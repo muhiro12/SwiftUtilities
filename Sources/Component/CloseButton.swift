770f9c0be23e812484c55e6ed757394bd9b0c37f
muhiro_ <muhiro.12@gmail.com>
2024-06-29 09:00:38 +0900

    Add Component

---
+++ Sources/Component/CloseButton.swift
@@ -0,0 +1,31 @@
+//
+//  CloseButton.swift
+//  
+//
+//  Created by Hiromu Nakano on 2024/06/29.
+//
+
+import SwiftUI
+
+public struct CloseButton: View {
+    @Environment(\.dismiss) private var dismiss
+
+    public init() {}
+
+    public var body: some View {
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: "xmark.circle.fill")
+                .foregroundStyle(
+                    Color(.secondaryLabel),
+                    Color(.secondarySystemFill)
+                )
+                .font(.title3)
+        }
+    }
+}
+
+#Preview {
+    CloseButton()
+}
