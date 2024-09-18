3d1659b7682df1de4a8ba74eb53d3d3049c86a07
muhiro_ <muhiro.12@gmail.com>
2024-09-18 15:12:40 +0900

    Update normalizedContains

---
+++ Sources/Extension/StringProtocolExtension.swift
@@ -0,0 +1,20 @@
+//
+//  StringProtocolExtension.swift
+//  SwiftUtilities
+//
+//  Created by Hiromu Nakano on 9/18/24.
+//
+
+public extension StringProtocol {
+    func normalizedContains<T>(_ other: T) -> Bool where T : StringProtocol {
+        let normalizedSelf = self
+            .applyingTransform(.fullwidthToHalfwidth, reverse: false)?
+            .applyingTransform(.hiraganaToKatakana, reverse: false) ?? .empty
+
+        let normalizedOther = other
+            .applyingTransform(.fullwidthToHalfwidth, reverse: false)?
+            .applyingTransform(.hiraganaToKatakana, reverse: false) ?? .empty
+
+        return normalizedSelf.localizedStandardContains(normalizedOther)
+    }
+}
