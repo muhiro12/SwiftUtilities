1fbc553b85df20b44f33c16f8d606195e8686044
muhiro_ <muhiro.12@gmail.com>
2024-10-12 03:18:10 +0900

    Add ModelContextExtension

---
+++ Sources/Extension/ModelContextExtension.swift
@@ -0,0 +1,26 @@
+//
+//  ModelContextExtension.swift
+//  SwiftUtilities
+//
+//  Created by Hiromu Nakano on 10/12/24.
+//
+
+import SwiftData
+
+public extension ModelContext {
+    func fetchFirst<T>(_ descriptor: FetchDescriptor<T>) throws -> T? where T : PersistentModel {
+        var descriptor = descriptor
+        descriptor.fetchLimit = 1
+        return try fetch(descriptor).first
+    }
+
+    func random<T>(_ descriptor: FetchDescriptor<T>) throws -> T? where T : PersistentModel {
+        let count = try fetchCount(descriptor)
+        let offset = Int.random(in: 0..<count)
+
+        var descriptor = descriptor
+        descriptor.fetchOffset = offset
+
+        return try fetchFirst(descriptor)
+    }
+}
