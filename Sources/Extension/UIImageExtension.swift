a991fbceda3cb9363c54830977da57e3e826313b
muhiro_ <muhiro.12@gmail.com>
2025-04-24 19:05:50 +0900

    Add UIImage.appIcon to retrieve app icon from bundle safely

---
+++ Sources/Extension/UIImageExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UIImageExtension.swift
+//  SwiftUtilities
+//
+//  Created by Hiromu Nakano on 2025/04/24.
+//
+
+import UIKit
+
+extension UIImage {
+    static var appIcon: UIImage {
+        guard let icons = Bundle.main.infoDictionary?["CFBundleIcons"] as? [String: Any],
+              let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+              let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+              let iconName = iconFiles.last else {
+            return .init()
+        }
+        return .init(named: iconName) ?? .init()
+    }
+}
