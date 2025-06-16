ee509f9115c832fb5296fb963c7d9f50a316f32d
muhiro_ <muhiro.12@gmail.com>
2025-06-17 04:18:57 +0900

    Add Base64 encode/decode utilities to PersistentIdentifier with error handling

---
+++ Sources/Model/SwiftUtilitiesError.swift
@@ -0,0 +1,12 @@
+//
+//  SwiftUtilitiesError.swift
+//  SwiftUtilities
+//
+//  Created by Hiromu Nakano on 2025/06/17.
+//
+
+import Foundation
+
+public enum SwiftUtilitiesError: Error {
+    case invalidBase64String
+}
