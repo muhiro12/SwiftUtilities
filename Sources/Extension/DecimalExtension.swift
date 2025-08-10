ce7b3189527af0e90e79587800769c948d8e3132
muhiro_ <muhiro.12@gmail.com>
2025-08-10 22:45:56 +0900

    Add Calendar Date DateFormatter Decimal and String extensions under Sources/Extension

---
+++ Sources/Extension/DecimalExtension.swift
@@ -0,0 +1,19 @@
+//
+//  DecimalExtension.swift
+//  SwiftUtilities
+//
+//  Created by Hiromu Nakano on 2020/06/24.
+//  Copyright © 2020 Hiromu Nakano. All rights reserved.
+//
+
+import SwiftUI
+
+public nonisolated extension Decimal {
+    var isPlus: Bool {
+        self > .zero
+    }
+
+    var isMinus: Bool {
+        self < .zero
+    }
+}
